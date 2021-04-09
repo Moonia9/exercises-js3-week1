eeacecf6ccb8fcd4e743484e57c24dad796de983
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-04-09 18:20:47 +0200

    updated exercise1.js/B-tracing-code/InClass

--- InClass/A-ES6/exercise2.js
+++ InClass/A-ES6/exercise2.js
@@ -5,14 +5,13 @@ class Polygon {
     this.width = width;
   }
   sayName() {
-    console.log('Hi, I am a ' + this.name);
+    console.log("Hi, I am a " + this.name);
   }
 }
 
-let p = new Polygon('Polygon', 300, 400);
+let p = new Polygon("Polygon", 300, 400);
 p.sayName();
-console.log('The area of this polygon is ' + p.area);
-
+console.log("The area of this polygon is " + p.area);
 
 class Rectangle extends Polygon {
   constructor(height, width) {
@@ -21,19 +20,58 @@ class Rectangle extends Polygon {
     this.sides = 4;
     //this.area = this.height * this.width;
   }
-  get area() {return this.width * this.height;}
+  get area() {
+    return this.width * this.height;
+  }
   sayName() {
-    console.log('Hi I am a polygon and my name is ' + this.name + '.');
+    console.log("Hi I am a polygon and my name is " + this.name + ".");
   }
 }
 
 let r = new Rectangle(50, 60);
 r.sayName();
 //r.area = 3;
-console.log('The area of this polygon is ' + r.area);
+console.log("The area of this polygon is " + r.area);
 
 //Create 2 new classes that extend from polygon: triangle and circle and log their names and areas
 
+//Triangle Class
+class Triangle extends Polygon {
+  constructor(height, width) {
+    super(height, width);
+    this.name = "Triangle";
+    this.sides = 3;
+    //this.area = (this.height * this.width) / 2;
+  }
+  get area() {
+    return (this.width * this.height) / 2;
+  }
+  sayName() {
+    console.log("Hi I am a polygon and my name is " + this.name + ".");
+  }
+}
 
+let t = new Triangle(50, 60);
+t.sayName();
+console.log("The area of this polygon is " + t.area);
 
+//Circle Class
+class Circle extends Polygon {
+  constructor(diameter, radius) {
+    super(diameter, radius);
+    this.diameter = 2 * radius;
+    this.name = "Circle";
+    //this.sides = 0;
+    //this.area = (Math.sqrt(radius) * 22/ 7;
+  }
+  get area() {
+    return (Math.sqrt(this.radius) * 22) / 7;
+  }
+  sayName() {
+    console.log("Hi I am a polygon and my name is " + this.name + ".");
+  }
+}
 
+let c = new Circle(30, 180);
+c.sayName();
+console.log("The area of this polygon is " + c.area);
--- InClass/B-Tracing code/exercise1/exercise1.js
+++ InClass/B-Tracing code/exercise1/exercise1.js
@@ -1,19 +1,26 @@
 function greetPeople(people) {
-  var greeting = "He";
-  greeting = greeting + "llo";
+  var greeting = "Hello";
   greeting = greeting + " ";
+  // greeting = greeting + "llo";
+  // greeting = greeting + " ";
 
-  people.forEach(function(person) {
+  people.forEach(function (person) {
     greeting = greeting + person;
-	console.log(greeting);
+    console.log(greeting);
   });
 
   return greeting;
 }
 
+/*The console logs:
+Hello Irina
+Hello IrinaAshleigh
+Hello IrinaAshleighEtza
+*/
+
 /*
   Let's trace this piece of code - what is the value of result with this input
   
   */
-  var mentors = ['Irina', 'Ashleigh', 'Etza'];
-  var result = greetPeople(mentors)
+var mentors = ["Irina", "Ashleigh", "Etza"];
+var result = greetPeople(mentors);
