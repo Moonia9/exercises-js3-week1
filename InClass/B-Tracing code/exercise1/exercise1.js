84b008729e16f9c29d5370ec8a5de62279689062
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-04-12 09:26:07 +0200

    updated exercise1.js/B-Tracing

--- InClass/B-Tracing code/exercise1/exercise1.js
+++ InClass/B-Tracing code/exercise1/exercise1.js
@@ -1,23 +1,35 @@
 function greetPeople(people) {
   var greeting = "Hello";
   greeting = greeting + " ";
-  // greeting = greeting + "llo";
-  // greeting = greeting + " ";
+  greeting = greeting + "llo";
+  greeting = greeting + " ";
 
-  people.forEach(function (person) {
-    greeting = greeting + person;
-    console.log(greeting);
-  });
+  // people.forEach((person) =>  {
+  //   greeting = greeting + person;
+  //   greeting = `${greeting} ${person}`;
+  //   console.log(greeting);
+  // });
+
+  for (const key in people) {
+    const person = people[key];
+    greeting = `${greeting} ${person},`;
+    console.log(key);
+
+    if(key === people.length - 1){
+      greeting = `${greeting} ${person},`;
+    } else if (key === people.length - 1) {
+      greeting = `${greeting} ${person} & `;
+    } else {
+      greeting = `${greeting} ${person}`;
+    }
+  }
 
   return greeting;
+  
 }
 
 console.log(result);
-/*The console logs:
-Hello Irina
-Hello IrinaAshleigh
-Hello IrinaAshleighEtza
-*/
+
 
 /*
   Let's trace this piece of code - what is the value of result with this input
