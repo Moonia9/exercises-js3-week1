b840ec94f097bb221751dabe72ec97d1e9845cc1
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-04-14 18:32:20 +0200

    updated exercise1.js/B-TracingCode

--- InClass/B-Tracing code/exercise1/exercise1.js
+++ InClass/B-Tracing code/exercise1/exercise1.js
@@ -4,32 +4,23 @@ function greetPeople(people) {
   greeting = greeting + "llo";
   greeting = greeting + " ";
 
-  // people.forEach((person) =>  {
-  //   greeting = greeting + person;
-  //   greeting = `${greeting} ${person}`;
-  //   console.log(greeting);
-  // });
-
   for (const key in people) {
     const person = people[key];
     greeting = `${greeting} ${person},`;
     console.log(key);
-
-    if(key === people.length - 1){
+    if (parseInt(key) === people.length - 1) {
       greeting = `${greeting} ${person},`;
-    } else if (key === people.length - 1) {
+    } else if (parseInt(key) === people.length - 1) {
       greeting = `${greeting} ${person} & `;
     } else {
-      greeting = `${greeting} ${person}`;
+      greeting = `${greeting} ${person},`;
     }
   }
 
   return greeting;
-  
 }
 
 console.log(result);
-
 
 /*
   Let's trace this piece of code - what is the value of result with this input
